dc082703380a19522fe699ef57aca92d6e712738
shux <shu1@XiangdeMacBook-Pro.local>
2019-05-06 18:20:22 -0400

    1

---
+++ 3page/js/main.js
@@ -0,0 +1,20 @@
+$(".nav--icon").click(function(){
+    $(".overlay").addClass
+    ('open');
+  });
+  
+  $(".overlay-close").click(function(){
+    $(".overlay").removeClass
+    ('open');
+  });
+  
+  
+  $(".intro").click(function(){
+    $(".overlay2").addClass
+    ('open');
+  });
+  
+  $(".overlay-close2").click(function(){
+    $(".overlay2").removeClass
+    ('open');
+  });
--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+$(window).load(function() {
+	setTimeout(function() { // Remove this on production
+		$("#preloader").fadeOut();
+		setTimeout(function() {
+			$("#preloader").remove();
+		}, 500);
+	}, 2500);
+});
+
+
 $(".nav--icon").click(function(){
   $(".overlay").addClass
   ('open');
@@ -7,5 +17,21 @@ $(".overlay-close").click(function(){
   $(".overlay").removeClass
   ('open');
 });
+
+
+$(".intro").click(function(){
+  $(".overlay2").addClass
+  ('open');
+});
+
+$(".overlay-close2").click(function(){
+  $(".overlay2").removeClass
+  ('open');
+});
+
+
+
+
+
 
 
